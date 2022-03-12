842f3e043bfc71489290c958d616a8403e4620a9
Vikrant Balwant Nikumbhe <94373839+Vikrantnikumbhe@users.noreply.github.com>
2022-03-13 00:18:37 +0530

    Update app_crm.py

--- app_crm.py
+++ app_crm.py
@@ -119,8 +119,8 @@ import streamlit.components.v1 as components
 
 col6,col3, col4, col5 = st.columns([0.5,1.5,0.5,3])
 with col3:
-  img = Image.open("./images/grahak-360.png")
-  st.image(img,caption = 'WALCHAND COLLEGE OF ENGINEERING,SANGLI',use_column_width='auto')
+  imgt = Image.open("./images/grahak-360.PNG")
+  st.image(imgt,,use_column_width='auto')
 with col5:
   j = '''Attention decision-makers! Tired of cluttering through oodles
 of spreadsheets?Looking for a comprehensive gizmo to alleviate 
